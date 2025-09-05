d1826ab908e584527c32aa32e755e34a8ce5a5a5
Agshin Ismayilzada <agshinzada@gmail.com>
2025-09-06 00:00:34 +0400

    update project page

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -2,32 +2,34 @@ import { supabase } from "utils/supabase";
 import PortfolioItem from "./PortfolioItem";
 import { useEffect, useState } from "react";
 
-const PortfolioSection = ({ slice = true }) => {
+const PortfolioSection = () => {
   const [projects, setProjects] = useState([]);
   const getData = async () => {
     const { data, error } = await supabase
       .from(process.env.REACT_APP_PROJECT_VIEW)
       .select("*")
+      .eq("type", 1)
       .order("sort", { ascending: false });
     if (error) {
       console.log(error);
     }
     setProjects(data);
   };
+
   useEffect(() => {
     getData();
   }, []);
 
   return (
-    <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {projects
-        .slice(0, slice ? 6 : projects.length) // Eğer slice gerekiyorsa ilk 6 eleman
-        .map((item) => (
+    <div>
+      <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {projects.slice(0, 6).map((item) => (
           <li key={item.id}>
             <PortfolioItem data={item} />
           </li>
         ))}
-    </ul>
+      </ul>
+    </div>
   );
 };
 
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,9 +1,41 @@
-import PortfolioSection from "components/PortfolioSection";
+import PortfolioItem from "components/PortfolioItem";
+import { useEffect, useState } from "react";
+import { FaExternalLinkSquareAlt } from "react-icons/fa";
 import { IoArrowBackCircle } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
+import { supabase } from "utils/supabase";
 
 const ProjectsPage = () => {
   const navigate = useNavigate();
+  const [projects, setProjects] = useState([]);
+  const [otherProjects, setOtherProjects] = useState([]);
+  const getData = async () => {
+    const { data, error } = await supabase
+      .from(process.env.REACT_APP_PROJECT_VIEW)
+      .select("*")
+      .eq("type", 1)
+      .order("sort", { ascending: false });
+    if (error) {
+      console.log(error);
+    }
+    setProjects(data);
+  };
+
+  const getOtherData = async () => {
+    const { data, error } = await supabase
+      .from(process.env.REACT_APP_PROJECT_VIEW)
+      .select("*")
+      .eq("type", 2)
+      .order("sort", { ascending: false });
+    if (error) {
+      console.log(error);
+    }
+    setOtherProjects(data);
+  };
+  useEffect(() => {
+    getData();
+    getOtherData();
+  }, []);
 
   return (
     <div className="bg-[#fcfcfc] dark:bg-primaryDarkColor transition-colors duration-500 dark:border-t-secondaryDarkColor dark:border-t">
@@ -25,7 +57,54 @@ const ProjectsPage = () => {
         </p>
       </div>
       <div className="px-6 md:px-20 py-2">
-        <PortfolioSection slice={false} />
+        <div>
+          <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {projects.map((item) => (
+              <li key={item.id}>
+                <PortfolioItem data={item} />
+              </li>
+            ))}
+          </ul>
+          <div>
+            <h2 className="text-2xl mt-10 mb-5 dark:text-white">
+              Other projects
+            </h2>
+            <ul className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+              {otherProjects.map((item) => (
+                <li key={item.id}>
+                  <div className="border py-3 px-4 rounded-lg flex md:flex-row flex-col gap-2 md:justify-between md:items-center bg-white dark:bg-fourdDarkColor">
+                    <div>
+                      <h5 className="font-semibold text-lg">{item.title}</h5>
+                      <p className="text-xs md:text-sm text-gray-500 mb-2">
+                        {item.description}
+                      </p>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                      <div>
+                        {item.tags.split(",").map((tag, index) => (
+                          <span
+                            key={index}
+                            class="bg-blue-100 text-blue-800 text-xs font-semibold me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300"
+                          >
+                            {tag}
+                          </span>
+                        ))}
+                      </div>
+                      <a
+                        href={item.repo}
+                        className="flex items-center gap-2"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Github <FaExternalLinkSquareAlt />
+                      </a>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
